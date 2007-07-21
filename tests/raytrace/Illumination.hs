c962c4f6133adb551ba8f138db3830a5349573c3
andy@galois.com <unknown>
2007-07-21 21:07:01 +0000

    fixing hpcs raytrace test to work with and without opt
    The old problem seems to be to do with rounding in floating point
    number. This gets round this using a post compare, but also
    for some reason also fixes the problem completely. Ugg. Floating point.

--- tests/raytrace/Illumination.hs
+++ tests/raytrace/Illumination.hs
@@ -30,8 +30,16 @@ render :: (Matrix,Matrix) -> Color -> [Light] -> Object -> Int ->
           Radian -> Int -> Int -> String -> IO ()
 render (m,m') amb ls obj dep fov wid ht file
   = do { debugging
-       ; putStrLn (showBitmap' wid ht pixels)
-       }
+       ; txt <- readFile "galois.sample"
+       ; let vals = read txt
+       ; let rt_vals = showBitmap' wid ht pixels
+       ; if length vals /= length rt_vals
+           then print ("BAD LENGTH",length vals,length rt_vals)
+           else do {
+                   ; let cmp = sum(zipWith (\ a b -> abs (a - b) * abs (a - b)) vals rt_vals)
+                   ; print $ if cmp == 0 then ("GOOD MATCH") else ("BAD MATCH:" ++ show cmp)
+                   }}
+                   
   where
     debugging = return ()
 {-
@@ -130,19 +138,18 @@ showBitmap _ _ _ = error "incorrect length of bitmap string"
 scalePixel :: Double -> Char
 scalePixel p = chr (floor (clampf p * 255))
 
-showBitmap' :: Int -> Int ->[[Color]] -> String
+showBitmap' :: Int -> Int ->[[Color]] -> [Int]
 showBitmap' wid ht pss
 -- type of assert  | length pss == ht && all (\ ps -> length ps == wid) pss
-  = header
- ++ unlines [ unwords [unwords [scalePixel' r,scalePixel' g,scalePixel' b]
-                      | (r,g,b) <- map uncolor ps]
-            | ps <- pss ]
+  = concat [ concat [  [scalePixel' r,scalePixel' g,scalePixel' b]
+                    | (r,g,b) <- map uncolor ps]
+           | ps <- pss ]
   where
     header = "P3\n#Galois\n" ++ show wid ++ " " ++ show ht ++ "\n255\n"
 showBitmap' _ _ _ = error "incorrect length of bitmap string"
 
-scalePixel' :: Double -> String
-scalePixel' p = show (floor (clampf p * 255))
+scalePixel' :: Double -> Int
+scalePixel' p = floor (clampf p * 255)
 
 -- Lights
 
