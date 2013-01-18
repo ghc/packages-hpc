a433ac28e50873ff7f61fd0f837e2722246222b4
Ian Lynagh <ian@well-typed.com>
2013-01-18 11:46:49 +0000

    Fix tests when BINDIST=YES

--- tests/hpcrun.pl
+++ tests/hpcrun.pl
@@ -33,7 +33,7 @@ system ($HPC, "report", "$binary.tix");
 print "\n\n";
 system ($HPC, "report", "$binary.tix", "--per-module");
 print "\n\n";
-open(MARKUP, "-|", "$HPC markup $binary.tix");
+open(MARKUP, "-|", $HPC, "markup", "$binary.tix");
 while(<MARKUP>) {
   my $line = $_;
   print $line;
